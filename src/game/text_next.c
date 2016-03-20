5f4c0cddf5315ccefd5a7bc428384121949bbabd
bougon_p <bougon_p@epitech.eu>
2016-03-20 23:38:53 +0100

    final

--- src/game/text_next.c
+++ src/game/text_next.c
@@ -5,7 +5,7 @@
 ** Login   <duhieu_b@epitech.net>
 **
 ** Started on  Sun Mar 20 22:48:04 2016 benjamin duhieu
-** Last update Sun Mar 20 22:54:21 2016 benjamin duhieu
+** Last update Sun Mar 20 23:38:50 2016 bougon_p
 */
 
 #include "mega.h"
@@ -29,7 +29,7 @@ t_text		*char_to_list(char *txt)
   t_text	*text;
   int		i;
 
-  if (!(text = malloc(sizeof(t_text))))
+  if (!(text = bunny_malloc(sizeof(t_text))))
     return (NULL);
   text->next = NULL;
   elem = text;
@@ -40,7 +40,7 @@ t_text		*char_to_list(char *txt)
 	fill_list(elem, txt[i], i);
       else
 	fill_list(elem, txt[i], i + 1);
-      if (!(elem->next = malloc(sizeof(t_text))))
+      if (!(elem->next = bunny_malloc(sizeof(t_text))))
 	return (NULL);
       elem = elem->next;
     }
